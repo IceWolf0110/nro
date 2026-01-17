b9e9cd51509d3442844c7a8c56e6adc45842e3c9
icewolf0110 <icewolf0110@gmail.com>
2026-01-17 13:15:40 +0700

    init backend

---
+++ server/src/test/kotlin/com/nro/nro/NroApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nro.nro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NroApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
